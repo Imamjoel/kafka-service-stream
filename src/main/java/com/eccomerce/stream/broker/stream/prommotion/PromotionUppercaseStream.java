f47f9e5d7bf6726c6b1d3aad5a6fdd0e0caa812d
Imamjoel <mfahreinheitz@gmail.com>
2021-04-24 23:40:04 +0700

    Initial commit

---
+++ src/main/java/com/eccomerce/stream/broker/stream/prommotion/PromotionUppercaseStream.java
@@ -0,0 +1,29 @@
+package com.eccomerce.stream.broker.stream.prommotion;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PromotionUppercaseStream {
+
+    @Bean
+    public KStream<String, String> kStreamPromotoion(StreamsBuilder builder)
+    {
+        KStream<String, String> sourceStream = builder.stream("t.commodity.promotion", Consumed.with(Serdes.String(),Serdes.String()));
+
+        KStream<String, String> uppercaseStream = sourceStream.mapValues(s -> s.toUpperCase());
+        uppercaseStream.to("t.commodity.promotion-uppercase");
+
+        sourceStream.print(Printed.<String, String>toSysOut().withLabel("original stream"));
+        uppercaseStream.print(Printed.<String, String>toSysOut().withLabel("uppercase stream"));
+
+        return sourceStream;
+
+    }
+}
